6024892279203d2d0d41ae6cbd5b544350d774b0
Praveen-Aanand <praveenanand2261999@gmail.com>
2022-03-20 14:53:52 +0530

    first commit

---
+++ lib/simple_pagination.dart
@@ -0,0 +1,183 @@
+library simple_pagination;
+
+import 'package:flutter/material.dart';
+
+class SimplePagenation extends StatefulWidget {
+  final List<Widget> children;
+  final int itemsPerPage;
+  final Icon nextIcon, previousIcon;
+  final Decoration decoration, currentPageDecoration;
+  final EdgeInsetsGeometry padding;
+  final TextStyle textStyle;
+  const SimplePagenation(
+      {Key? key,
+      required this.children,
+      this.itemsPerPage: 5,
+      this.nextIcon: const Icon(Icons.arrow_forward, size: 20),
+      this.previousIcon: const Icon(Icons.arrow_back, size: 20),
+      this.decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      this.currentPageDecoration: const BoxDecoration(
+        color: Colors.blueAccent,
+      ),
+      this.padding: const EdgeInsets.all(0.0),
+      this.textStyle: const TextStyle(fontSize: 18)})
+      : super(key: key);
+
+  @override
+  _SimplePagenationState createState() => _SimplePagenationState();
+}
+
+class _SimplePagenationState extends State<SimplePagenation> {
+  late int currentPage = 0, showFrom, showTo, numberOfPages, maxNumOfPages = 4;
+  late List<Widget> lastState;
+  _update() {
+    setState(() {
+      lastState = widget.children;
+      showFrom = 0;
+      showTo = widget.itemsPerPage >= widget.children.length
+          ? widget.children.length
+          : widget.itemsPerPage;
+      numberOfPages = (widget.children.length / widget.itemsPerPage).ceil();
+    });
+    if (currentPage <= numberOfPages) {
+      _moveTo(currentPage);
+    } else {
+      _moveTo(0);
+    }
+  }
+
+  _moveTo(index) {
+    if (index >= 0 && index <= numberOfPages - 1) {
+      setState(() {
+        showFrom = index * widget.itemsPerPage;
+        if (showFrom + widget.itemsPerPage <= widget.children.length)
+          showTo = showFrom + widget.itemsPerPage;
+        else
+          showTo = widget.children.length;
+        currentPage = index;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _update();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (lastState != widget.children) {
+      _update();
+    }
+    if ((showTo > 0 && widget.children.length > 0)) {
+      return Container(
+        child: Column(
+          children: [
+            // slised list
+
+            ...widget.children.sublist(showFrom, showTo),
+
+            // nav
+            Container(
+              alignment: Alignment.centerRight,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    // previous Button
+                    InkWell(
+                      onTap: () {
+                        _moveTo(currentPage - 1);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(6.0),
+                        height: 30,
+                        decoration: widget.decoration,
+                        child: widget.previousIcon,
+                      ),
+                    ),
+                    //page numbers list
+                    ...List.generate(
+                      numberOfPages,
+                      (index) => index,
+                    ).map((index) => InkWell(
+                          onTap: () {
+                            _moveTo(index);
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(6.0),
+                            decoration: (currentPage == index)
+                                ? widget.currentPageDecoration
+                                : widget.decoration,
+                            height: 30,
+                            child: Text(
+                              (index + 1).toString(),
+                              style: widget.textStyle,
+                            ),
+                          ),
+                        )),
+                    // next Button
+                    InkWell(
+                      onTap: () {
+                        _moveTo(currentPage + 1);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(6.0),
+                        height: 30,
+                        decoration: widget.decoration,
+                        child: widget.nextIcon,
+                      ),
+                    ),
+                  ]
+                      .map((e) => Padding(
+                            padding: widget.padding,
+                            child: e,
+                          ))
+                      .toList(),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return Text("List is empty");
+    }
+  }
+}
+
+extension FocusCrop on List {
+  List focusCrop(currentIndex, int length) {
+    int start, end = 0;
+    if (currentIndex - length ~/ 2 > 0) {
+      start = currentIndex - length ~/ 2;
+    } else {
+      start = 0;
+    }
+
+    if ((currentIndex + length ~/ 2) + 1 < this.length) {
+      end = currentIndex + length ~/ 2;
+    } else {
+      end = this.length - 1;
+    }
+    return this.sublist(start, end + 1);
+  }
+
+  List crop(currentIndex, int length) {
+    int start = currentIndex, end = currentIndex;
+    List<int> list = [currentIndex];
+    for (var i = length; i > 1; i--) {
+      if (i % 2 == 0 && list.length <= this.length) {
+        start--;
+        list.insert(0, start);
+      } else if (list.length <= this.length) {
+        end++;
+        list.add(end);
+      }
+    }
+    return list;
+  }
+}
---
+++ test/simple_pagination_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:simple_pagination/simple_pagination.dart';
+
+void main() {
+ 
+}
